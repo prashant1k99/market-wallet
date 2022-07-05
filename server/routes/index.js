4e06a7d56f77c7ec17bdc38a08f853b8b28d8c08
Prashant <prashantco111@gmail.com>
2022-07-05 19:36:37 +0530

    WIP: added middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,16 @@ const express = require('express')
 const userRoute = require('./user')
 const adminRoute = require('./admin')
 
+const {
+  hasAdminAccess,
+  authenticate
+} = require('../middleware')
+
 const app = express()
 app.use(express.json())
 app.disable('x-powered-by')
 
-app.use('/user', userRoute)
-app.use('/admin', adminRoute)
+app.use('/user', authenticate, hasAdminAccess, userRoute)
+app.use('/admin', authenticate, adminRoute)
 
 module.exports = app
